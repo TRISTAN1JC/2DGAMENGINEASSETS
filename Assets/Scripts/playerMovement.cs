b2ebf3c29d52b9c5b381b5423ce5530a7d08906c
TRISTAN1JC <N00254650@iadt.ie>
2026-01-28 15:55:48 +0000

    new code for anim

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -13,11 +13,13 @@ public class PlayerMovement : MonoBehaviour
     private float horizInput;
     private bool isGrounded;
     private SpriteRenderer spriteRenderer;
+    private AnImator AnImator
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anImator = GetComponent<AnImator>();
     }
 
     void Update()
@@ -37,6 +39,12 @@ public class PlayerMovement : MonoBehaviour
         {
             if (horizInput > 0.1f) spriteRenderer.flipX = false;
             else if (horizInput < -0.1f) spriteRenderer.flipX = true;
+        }
+
+        if (animator != null)
+        {
+            animator.SetFloat("moveInput", Mathf.Abs(horizInput));
+            animator.SetBool("isGrounded", isGrounded);
         }
     }
   void FixedUpdate()
