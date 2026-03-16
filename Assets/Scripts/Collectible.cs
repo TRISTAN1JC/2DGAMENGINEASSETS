9ea1b0674697bb88c405dae6fa2f55a23c3e972a
TRISTAN1JC <N00254650@iadt.ie>
2026-03-16 16:08:13 +0000

    Shader animations code+Downaloaded stuff

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 90f;
+   // [SerializeField]
     private void Update()
     {
         transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       /* if (pickupEffectPrefab != null)
+    {
+       ParticleSystem effect = Instantiate(pickupEffectPrefab, transform.position, Quaternion.identity);
+       effect.Play();
+
+        //optional
+        //Destroy.gameObject, effect.mainduration +
+        } */
         if (collision.CompareTag("Player"))
         {
             AudioManager.Instance.PlaySFX(AudioManager.Instance.collectibleSFX);
